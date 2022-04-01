fe1fb4104358873a6cb815243cccb318da8372af
Leferchian <leferchian@gmail.com>
2022-04-01 20:32:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 7005;
+        int bonusMiles;
+
+        bonusMiles = ticketPrice / 20;
+        System.out.println(bonusMiles);
+    }
+}
